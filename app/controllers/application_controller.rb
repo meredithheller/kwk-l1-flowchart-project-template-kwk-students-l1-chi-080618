b343d606ab324d3e8d5000feba1a7b7b4be15f14
meredithheller <meredithheller@outlook.com>
2018-08-16 16:02:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get '/' do
+    erb :index
+  end 
+  
+  get '/quiz' do
+    erb :quiz
+  end 
 
 end
